18eda843fc64b838e510ca97d01c0b3088f7b1f1
paul-sf <paul-sf@1474dadc-8ad6-4956-966a-57cce2b732df>
2005-03-22 09:17:12 +0000

    *** empty log message ***

--- trunk/keow/make-release.sh
+++ trunk/keow/make-release.sh
@@ -9,9 +9,10 @@
 
 rm -f keow.zip
 [ ! -d keow ] && mkdir keow
-cp Release/*.exe Release/*.dll keow
+cp Release/*.exe Release/*.dll INSTALL-keow.txt keow
 zip -r9 keow.zip keow
 
-rm -f bootstrap-linux.zip
-(cd bootstrap; find linux | grep -v '/CVS' | zip -9y ../bootstrap-linux.zip -@ )
-zip bootstrap-linux.zip keow-bash.cmd
+#do this manually - we need to keep the windows .lnk file intact
+#rm -f bootstrap-linux.zip
+#zip bootstrap-linux.zip INSTALL-bootstrap.txt keow-bash.cmd
+#(cd bootstrap; find linux | grep -v '/CVS' | zip -9 ../bootstrap-linux.zip -@ )
