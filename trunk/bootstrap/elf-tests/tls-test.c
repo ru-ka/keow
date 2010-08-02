e210b0403f4bd61090c05496cd8556741791d7ee
paul-sf <paul-sf@1474dadc-8ad6-4956-966a-57cce2b732df>
2010-08-02 03:26:56 +0000

    Test program changes to try to diagnose loading errors for 2.6 library code

--- trunk/bootstrap/elf-tests/tls-test.c
+++ trunk/bootstrap/elf-tests/tls-test.c
@@ -1,4 +1,7 @@
-/* a simple C testbed with NO c runtime to test api calls */
+/*
+  A testbed for thread-local-storage (TLS) APIs.
+  Does not use any libraries or libc/libgcc builtins (so we control all kernel api calls)
+*/
 
 #include <unistd.h>
 #include <linux/unistd.h>
@@ -20,6 +23,7 @@ int _write(int fd, char*s, int n)
     return 0;
 }
 
+//nodefaultlibs, so need to supply a syscall() system call
 int _syscall(int n, void*p)
 {
     asm (
@@ -30,11 +34,13 @@ int _syscall(int n, void*p)
     return 0;
 }
 
+//nodefaultlibs, so need to supply an exit() 
 void exit_n(int n) {
     _syscall(__NR_exit, (void*)n);
 }
 
 
+// Write a string to stdout
 void print(char *s)
 {
     int i=0;
@@ -42,6 +48,7 @@ void print(char *s)
     _write(1,s,i);
 }
 
+// Write a number to stdout
 void print_dec(long n)
 {
    char buf[20];
@@ -62,6 +69,7 @@ void print_dec(long n)
    _write(1,&buf[i],20-i);
 }
 
+// Write a number, in hex, to stdout
 void print_hex(unsigned long n)
 {
    char buf[20];
@@ -79,13 +87,20 @@ void print_hex(unsigned long n)
    _write(1,&buf[i],20-i);
 }
 
-int _start()
+
+/***************************************************************************/
+
+/* Test using a selector to access memory */
+void test_TLS(int granularity)
 {
     int selector;
     int i;
 
-	print("tls-test start\n");
+	print("Testing ");
+	print(granularity==0 ? "byte" : "page");
+	print(" granularity\n");
 
+    //init buffer with known values
     for(i=0; i<sizeof(buff); ++i) {
         buff[i] = i&0xFF;
     }
@@ -94,14 +109,14 @@ int _start()
     print("\n");
 
 
-    //Setup TLS
+    //Setup a TLS slot
 	ud.entry_number = -1;
 	ud.base_addr = (unsigned long)&buff;
 	ud.limit = sizeof(buff);
+	ud.limit_in_pages = granularity;
 	ud.seg_32bit = 1;
 	ud.contents = 0;
 	ud.read_exec_only = 0;
-	ud.limit_in_pages = 0;
 	ud.seg_not_present = 0;
 	ud.useable = 1;
     //set_thread_area(&buff);
@@ -114,25 +129,26 @@ int _start()
     print_dec(ud.entry_number);
     print("\n");
 
-    //assign to gs, and then use it
-    selector = (ud.entry_number<<3) | 0x3;
+    //assign to gs, and then use it (like libpthread does)
+    selector = (ud.entry_number<<3) | 0x3;  //GDT index at DPL=3
     print("selector ");
     print_hex(selector);
     print("\n");
     asm (
-        "movw %%ds, %%ax \n\t"
-        "movw %%ax, %%fs \n\t"
         "movl %0, %%eax \n\t"
         "movw %%ax, %%gs \n\t"
         :
         :"r"(selector)
-        :"%eax"
         );
+    //print again - testing keow's ability to preserve gs across api calls
     print("gs=");
     asm("movw %%gs, %%ax" :"=r"(i));
     print_hex(i);
     print("\n");
 
+
+    //Test that the selector works by accessing the data
+    //with both C and also by using the gs: selector.
 
     //read [13]
     print("buff[13]=");
@@ -142,8 +158,6 @@ int _start()
         "movl %%gs:13, %%eax \n\t"
         "movl %%eax, %0 \n\t"
         :"=r"(i)
-        :"r"(selector)
-        :"%eax"
         );
     print_hex(i);
     print("\n");
@@ -155,10 +169,16 @@ int _start()
     print_hex(buff[13]);
     print("\n");
 
+    print("\n");
+}
+/* this is main() when compiled with -nostartfiles */
+int _start()
+{
+	print("tls-test start\n");
 
+    test_TLS(0);
+    test_TLS(1);
 
 	print("end\n");
 	exit_n(0);
-
-    asm ( "pop %gs \n\t ret $16 \n\t" );
 }
